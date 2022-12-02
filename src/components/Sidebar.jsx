5af500b7b3f5e16fc44f50e2633100b599406e9e
Dylan Drebes <88466593+ddrebe473@users.noreply.github.com>
2022-12-02 15:03:46 -0700

    wip 4.0

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 const Sidebar = ({ buttons }) => {
     return (
         // <div className="sidebarComponent about-parent fixed top-0 left-0 bottom-0 h-screen bg-blue-700 w-80 flex flex-col items-center justify-evenly">
-        <div className="sidebarComponent bg-blue-700 w-40 flex flex-col items-center justify-evenly">
+        <div className="sidebarComponent bg-blue-700 w-40 flex flex-col items-center my-5 justify-evenly">
             {buttons}
         </div>
     )
