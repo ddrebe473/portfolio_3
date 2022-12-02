e324a7c15ef3875d53dc6557e7eb0e7916f9f106
luke <ltbsandiego@yahoo.com>
2022-12-01 22:16:53 -0700

    wip

--- src/components/Darkmode.jsx
+++ src/components/Darkmode.jsx
@@ -1,33 +1,38 @@
 import React from 'react'
 import { useState, useEffect } from "react";
+
+import '../style/darkmode.css'
+
 function DarkTheme() {
-  const [theme, setTheme] = useState("light");
-  const storedTheme = localStorage.getItem("Theme");
-  if (storedTheme == null) {
-    localStorage.setItem("Theme", "light");
-  }
-  // useEffect(() => {
-  //   setTheme(storedTheme);
-  // }, []);
+    const [theme, setTheme] = useState("light");
 
-  const handleClick = () => {
-    if (storedTheme === "light" || storedTheme === null) {
-      localStorage.removeItem("Theme");
-      localStorage.setItem("Theme", "dark");
-      setTheme("dark");
-    } else {
-      localStorage.removeItem("Theme");
-      localStorage.setItem("Theme", "light");
-      setTheme("Light");
+    const storedTheme = localStorage.getItem("Theme");
+
+    if (storedTheme == null) {
+        localStorage.setItem("Theme", "light");
     }
-  };
-  return (
-  <label className={`button-${theme} switch`} onClick={handleClick}>
-  <input type="checkbox"/>
-  <span className="slider round"></span>
-  </label>
+    
+    // useEffect(() => {
+    //   setTheme(storedTheme);
+    // }, []);
 
-  );
+    const handleClick = () => {
+        if (storedTheme === "light" || storedTheme === null) {
+            localStorage.removeItem("Theme");
+            localStorage.setItem("Theme", "dark");
+            setTheme("dark");
+        } else {
+            localStorage.removeItem("Theme");
+            localStorage.setItem("Theme", "light");
+            setTheme("Light");
+        }
+    };
+    return (
+        <label className={`button-${theme} switch`} onClick={handleClick}>
+            <input type="checkbox" />
+            <span className="slider round"></span>
+        </label>
+    );
 };
 
 export default DarkTheme
