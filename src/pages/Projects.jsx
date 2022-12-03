f3cefb573432e4f21e6bea0472372630ee3ee382
luke <ltbsandiego@yahoo.com>
2022-12-02 23:54:24 -0700

    wip

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useEffect } from 'react';
 import Sidebar from '../components/Sidebar'
 import TopBar from '../components/TopBar';
 
+import GithubHelper from '../util/githubHelper'
+
 function Projects() {
+
+    const [projects, setProjects] = React.useState([])
+
+    const loadProjects = async () => {
+        await GithubHelper.login();
+        const repos = await GithubHelper.getRepos();
+        setProjects(repos);
+    }        
+    
+    useEffect(() => {
+        loadProjects()
+    }, [])
+    
     const buttons = () => {
         return
     }
@@ -11,7 +27,14 @@ function Projects() {
         <TopBar />
         <Sidebar buttons={buttons()}/>
         <div className="mainContent">
-
+            {projects.map((project) => {
+                return <div className="projectRepo" onClick={() => {
+                    window.open(project.url.replace('//api.', '//www.').replace('repos/', ''));
+                }}>
+                    <h1>{project.name}</h1>
+                    <p>{project.description ?? 'Description'}</p>
+                </div>
+            })}
         </div>
     </>)
 }
---
+++ src/util/githubHelper.js
@@ -0,0 +1,37 @@
+
+
+import { Octokit, App } from "octokit";
+
+const TOKEN = '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'
+let owner = null
+
+const REPO_LIST = [
+    'tour_api',
+    'task-manager2',
+    'EnglishSpanishTranslation',
+    'Calculator',
+    'Crapstone',    
+]
+
+// Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
+const octokit = new Octokit({ auth: TOKEN });
+
+const login = async () => {
+    // Compare: https://docs.github.com/en/rest/reference/users#get-the-authenticated-user
+    const loginData = await octokit.rest.users.getAuthenticated();
+    owner = loginData.data.login
+    console.log("Hello, %s", loginData.data.login);
+}
+
+const getRepos = async (token) => {
+    const repoRes = await octokit.request(`GET /user/repos?visibility=public&sort=updated&per_page=100`, {})
+    console.log(repoRes)
+    return repoRes.data.filter(repo => REPO_LIST.includes(repo.name))
+}
+
+const githubHelper = {
+    login,
+    getRepos,
+};
+
+export default githubHelper;
